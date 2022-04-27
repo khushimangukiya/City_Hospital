eb2d7a2b17a249caab87ce2291f6423652bce695
khushimangukiya <khushimangukiya02@gmail.com>
2022-04-27 08:26:46 +0530

    login

--- src/Containers/LoginSignUp/LoginSignUp.js
+++ src/Containers/LoginSignUp/LoginSignUp.js
@@ -2,9 +2,58 @@ import React from 'react';
 
 function LoginSignUp(props) {
     return (
-        <div>
-            <p>data</p>
-        </div>
+        <section className='contact'>
+           <div className="container">
+                <div className="section-title">
+                    <h2>Sign Up</h2>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 mt-5 mt-lg-0">
+                        <form action method="post" role="form" className="php-email-form">
+                            <div className="justify-content-center row">
+                                <div className="col-md-7 form-group">
+                                    <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
+                                </div>
+                                <div className="col-md-7 form-group mt-3 mt-md-0">
+                                    <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+                                </div>
+                                <div className="col-md-7 form-group mt-3 mt-md-0">
+                                    <input type="password" className="form-control" name="password" id="password" placeholder="Enter Your Password" required />
+                                </div>
+                            </div>
+                            <div className="text-center mt-5 mb-5"><button type="submit">Sign Up</button></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="container">
+                <div className="section-title">
+                    <h2>Log In</h2>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 mt-5 mt-lg-0">
+                        <form action method="post" role="form" className="php-email-form">
+                            <div className="justify-content-center row">
+                                <div className="col-md-7 form-group mt-3 mt-md-0">
+                                    <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+                                </div>
+                                <div className="col-md-7 form-group mt-3 mt-md-0">
+                                    <input type="password" className="form-control" name="password" id="password" placeholder="Enter Your Password" required />
+                                </div>
+                            </div>
+                            <div className="text-center mt-5 mb-5"><button type="submit">Log In</button></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
     );
 }
 
--- src/Containers/MakeAnAppointment/MakeAnAppointment.js
+++ src/Containers/MakeAnAppointment/MakeAnAppointment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FormGroup, Input, Label } from 'reactstrap';
 
 function MakeAnAppointment(props) {
     return (
@@ -13,15 +14,40 @@ function MakeAnAppointment(props) {
                     <div className="col-lg-12 mt-5 mt-lg-0">
                         <form action method="post" role="form" className="php-email-form">
                             <div className="row">
-                                <div className="col-md-6 form-group">
+                                <div className="col-md-4 form-group">
                                     <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
                                 </div>
-                                <div className="col-md-6 form-group mt-3 mt-md-0">
+                                <div className="col-md-4 form-group mt-3 mt-md-0">
                                     <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
                                 </div>
+                                <div className="col-md-4 form-group mt-3 mt-md-0">
+                                    <input type="number" className="form-control" name="number" id="number" placeholder="Your Phone Number" required />
+                                </div>
                             </div>
-                            <div className="form-group mt-3">
-                                <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
+                            <div className='row'>
+                                <div className="form-group mt-3 col-md-4">
+                                    <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
+                                </div>
+                                <FormGroup className='col-md-4 mt-3 select_dropdown'>
+                                    <Input id="exampleSelect" name="select" type="select">
+                                    <option selected>Select Departments</option>
+                                        <option>
+                                        Departments 1
+                                        </option>
+                                        <option>
+                                        Departments 2
+                                        </option>
+                                        <option>
+                                        Departments 3
+                                        </option>
+                                        <option>
+                                        Departments 4
+                                        </option>
+                                        <option>
+                                        Departments 5
+                                        </option>
+                                    </Input>
+                                </FormGroup>
                             </div>
                             <div className="form-group mt-3">
                                 <textarea className="form-control" name="message" rows={5} placeholder="Message" required defaultValue={""} />
