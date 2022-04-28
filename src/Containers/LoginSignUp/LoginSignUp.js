fd72c090ac0277b9f85e7fca7a8cf96cac5a80bf
khushimangukiya <khushimangukiya02@gmail.com>
2022-04-28 09:05:10 +0530

    login with signup

--- src/Containers/LoginSignUp/LoginSignUp.js
+++ src/Containers/LoginSignUp/LoginSignUp.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function LoginSignUp(props) {
+    const [userType, setUserType] = useState('Login');
+    const [reset, setReset] = useState(false);
+     
+    const handalLogin = () => {
+
+    }
+    const handalsignUp = () => {
+        
+    }
+
+
     return (
         <section className='contact'>
-           <div className="container">
+            <div className="container">
                 <div className="section-title">
-                    <h2>Sign Up</h2>
+                    {
+                        reset ? <h2>Forgot Password</h2> :
+                        userType === 'Login' ? <h2>Log In</h2> 
+                        :
+                        <h2>Sign Up</h2>
+                    }
                 </div>
             </div>
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12 mt-5 mt-lg-0">
-                        <form action method="post" role="form" className="php-email-form">
+                        <div action method="post" role="form" className="php-email-form">
                             <div className="justify-content-center row">
+                                {
+                                    userType === 'Login'? null
+                                    :
                                 <div className="col-md-7 form-group">
                                     <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
                                 </div>
+                                }
                                 <div className="col-md-7 form-group mt-3 mt-md-0">
                                     <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
                                 </div>
@@ -23,37 +43,55 @@ function LoginSignUp(props) {
                                     <input type="password" className="form-control" name="password" id="password" placeholder="Enter Your Password" required />
                                 </div>
                             </div>
-                            <div className="text-center mt-5 mb-5"><button type="submit">Sign Up</button></div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+                            <div className="text-center mb-5">
+                                {
+                                     userType === 'Login'?
+                                     <div className='justify-content-end'>
+                                      <a href='#' onClick={() => setReset(true)}>Forgot Password ?</a>
+                                    </div>
+                                     :
+                                     null
+                                }
+                            </div>
+                            <div className="text-center mt-5 mb-2">
+                                {
+                                     userType === 'Login'? <button className='all_btns' onClick={() =>{handalLogin()}}>Login</button>
+                                     :
+                                    <button className='all_btns' onClick={() =>{handalsignUp()}}>Sign Up</button>
+                                }
+                            </div>
 
+                            <div className="text-center mb-5">
+                                {
+                                     userType === 'Login'?
+                                     <div>
+                                      <span className='me-2'>Create A New Account ?</span>
+                                      <a href='#' onClick={() => setUserType('signUp')}>SignUp</a>
+                                    </div>
+                                     :
+                                     <div>
+                                      <span className='me-2'>Already Have an Account?</span>
+                                      <a href='#' onClick={() => setUserType('Login')}>LogIn</a>
+                                    </div>
+                                }
+                            </div>
 
-            <div className="container">
-                <div className="section-title">
-                    <h2>Log In</h2>
-                </div>
-            </div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-12 mt-5 mt-lg-0">
-                        <form action method="post" role="form" className="php-email-form">
-                            <div className="justify-content-center row">
-                                <div className="col-md-7 form-group mt-3 mt-md-0">
+                            {/* <div className="text-center mb-5">
+                                {
+                                     reset === true ?
+                                     <div className="col-md-7 form-group mt-3 mt-md-0">
                                     <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
                                 </div>
-                                <div className="col-md-7 form-group mt-3 mt-md-0">
-                                    <input type="password" className="form-control" name="password" id="password" placeholder="Enter Your Password" required />
-                                </div>
-                            </div>
-                            <div className="text-center mt-5 mb-5"><button type="submit">Log In</button></div>
-                        </form>
+                                     :
+                                    null
+                                }
+                            </div> */}
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
-        
+
     );
 }
 
